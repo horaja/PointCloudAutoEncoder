91afe672f2784997aac130c7b365c57d208e2f96
horaja <horaja@andrew.cmu.edu>
2025-03-20 17:42:43 -0400

    Update Dataloaders.py

--- content/Dataloaders.py
+++ content/Dataloaders.py
@@ -1,55 +1,64 @@
-import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+import numpy as np
 import os
-import shutil
-        
-def clear_folder(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.mkdir(path)
+
+class ReadDataset(Dataset):
+    def __init__(self,  source):
+     
+        self.data = torch.from_numpy(source).float()
 
 
-def plotPCbatch(pcArray1, pcArray2, show = True, save = False, name=None, fig_count=9 , sizex = 12, sizey=3):
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+def RandomSplit(datasets, train_set_percentage):
+    lengths = [int(len(datasets)*train_set_percentage), len(datasets)-int(len(datasets)*train_set_percentage)]
+    return random_split(datasets, lengths)
+
+def GetDataLoaders(npArray, batch_size, train_set_percentage = 0.7, shuffle=True, num_workers=0, pin_memory=True):
     
-    pc1 = pcArray1[0:fig_count]
-    pc2 = pcArray2[0:fig_count]
     
-    fig=plt.figure(figsize=(sizex, sizey))
+    pc = ReadDataset(npArray)
+
+    train_set, test_set = RandomSplit(pc, train_set_percentage)
+
+    train_loader = DataLoader(train_set, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size, pin_memory=pin_memory)
+    test_loader = DataLoader(test_set, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size, pin_memory=pin_memory)
     
-    for i in range(fig_count*2):
+    return train_loader, test_loader
 
-        ax = fig.add_subplot(2,fig_count,i+1, projection='3d')
-        
-        if(i<fig_count):
-            ax.scatter(pc1[i,:,0], pc1[i,:,2], pc1[i,:,1], c='b', marker='.', alpha=0.8, s=8)
-        else:
-            ax.scatter(pc2[i-fig_count,:,0], pc2[i-fig_count,:,2], pc2[i-fig_count,:,1], c='b', marker='.', alpha=0.8, s=8)
-
-        ax.set_xlim3d(0.25, 0.75)
-        ax.set_ylim3d(0.25, 0.75)
-        ax.set_zlim3d(0.25, 0.75)
+def CondensePointClouds(input_folder, expected_num_points, x_range=(0,10), y_range=(-5,5), z_range=(-5,1)):
+    all_point_clouds = []
+    
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".npz"):
+            file_path = os.path.join(input_folder, filename)
+            data = np.load(file_path)
             
-        plt.axis('off')
-        
-    plt.subplots_adjust(wspace=0, hspace=0)
-        
-    if(save):
-        fig.savefig(name + '.png')
-        plt.close(fig)
+            point_cloud = data['points']
+            
+            mask = (
+                (point_cloud[:, 0] >= x_range[0]) & (point_cloud[:, 0] <= x_range[1]) &
+                (point_cloud[:, 1] >= y_range[0]) & (point_cloud[:, 1] <= y_range[1]) &
+                (point_cloud[:, 2] >= z_range[0]) & (point_cloud[:, 2] <= z_range[1])
+            )
+            
+            filtered_points = point_cloud[mask]
+            
+            if len(filtered_points) > expected_num_points:
+                sampled_indices = np.random.choice(len(filtered_points), expected_num_points, replace=False)
+                sampled_points = filtered_points[sampled_indices]
+            else:
+                print("Padding required - may need to modify expected num points")
+                padding_shape = (expected_num_points - len(filtered_points), 3)
+                sampled_points = np.pad(filtered_points, ((0, padding_shape[0]), (0,0)), 'constant', constant_values=0)
+                
+            all_point_clouds.append(sampled_points)
     
-    if(show):
-        plt.show()
-    else:
-        return fig
+    combined_data = np.stack(all_point_clouds, axis=0)
     
-def visualise_3(true_points, output_file=None):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim([0, 40])
-    ax.set_ylim([-20, 20])
-    ax.set_zlim([-15, 15])
-    ax.scatter(true_points[:, 0], true_points[:, 1], true_points[:, 2], s=1)
-
-    if (output_file):
-        fig.savefig(output_file)
-    
-    plt.show()
+    return combined_data
